b17d547ec1bb47fab0a0f933741ed5dff41d0903
Melina Estefania <meapilon@gmail.com>
2021-07-08 02:06:20 -0300

    TAREAS

---
+++ 02-JS-I/homework/02-JS-I-Teoria.js
@@ -0,0 +1,10 @@
+/* Variables: es una forma de declarar el valor de algo que va ser usado más tarde. Se declara con la palabra clave var y ahora saliendo otras que son: let (nivel de scope) y const (variable que no se puede cambiar, es constante)
+
+Strings: bloques de textos entre comillas
+
+Funciones (argumentos, return): las funciones son objetos invocables y se establece el nombre de la función entre paréntesis los argumentos (que serían las variables que le establecemos) y entre llaves el código return para que nos entregue el resultado que queremos, es decir que la función determine lo que está haciendo y te devuelve lo especificado. 
+
+Declaraciones if: es un operador de control de flujo y comparación, declaración que se usa para que la función verifique si es algo es true ejecutando un código que indique si es así o avanzando si no lo es. 
+
+Valores booleanos (true, false):  Indican verdadero o falso
+ */
---
+++ 03-JS-II/homework/03-JS-II-Teoria.js
@@ -0,0 +1,9 @@
+/* For: es un bucle que se usa para tareas repetitivas. For (Declaramos una variable; Expresión condicional; Incrementamos la variable con el operador ++ ) {código} Deja de funcionar cuando arroje false
+
+&&, ||, ¡: son operadores lógicos, dependiendo como lo uses que te indican si una situación es verdadera o falsa
+
+&&: Y (and): evalúa dos expresiones y devuelve verdadero si ambas son true o falso si una o ambas expresiones son false
+
+||: o (or): Determina si una de las expresiones es true: Devolverá true si una o ambas expresiones son true y devolverá false si ambas expresiones son falsas.
+
+¡: no ( NOT) de negación: Devuelve lo opuesto, si es true, devuelve false y viceversa. */
